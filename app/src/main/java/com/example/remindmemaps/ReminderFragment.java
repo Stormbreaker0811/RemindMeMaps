1a340ae40d63286b5df398ed21b9afc6b8123008
Omkar Chitnis <omkarchitnis1a@gmail.com>
2023-03-23 17:36:52 +0530

    Added views to settings fragment.

--- app/src/main/java/com/example/remindmemaps/ReminderFragment.java
+++ app/src/main/java/com/example/remindmemaps/ReminderFragment.java
@@ -84,7 +84,7 @@ public class ReminderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reminder, container, false);
-        maps = view.findViewById(R.id.mapView2);
+        //maps = view.findViewById(R.id.mapView2);
         //addReminder = view.findViewById(R.id.addReminder);
         // Inflate the layout for this fragment
         return view;
